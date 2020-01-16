244f27f3632b38488a3b8773a8b65b4083b61f2c
Adam <adam.dabrowski@outlook.com>
2020-01-16 21:20:51 +0100

    Api fetch stock prices

---
+++ src/api.ts
@@ -0,0 +1,36 @@
+import axios from 'axios';
+const API_URL = process.env.REACT_APP_API_URL;
+
+interface ServerResponse {
+  data: Array<TickerPrice>;
+}
+
+export interface TickerPrice {
+  name: string;
+  data: number;
+  open: string;
+  close: string;
+  volume: string;
+  max: string;
+  min: string;
+}
+
+export const getPrices = (ticker: string) => {
+  return axios
+    .get<ServerResponse>(`${API_URL}/stock-prices`, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+      },
+      proxy: {
+        host: '127.0.0.1',
+        port: 3000,
+      },
+      // TODO one day i will provide data, status and other, so simple r => r won't work
+      transformResponse: (r: Array<TickerPrice>) => r,
+    })
+    .then(response => {
+      return response.data;
+    });
+};
+
+export default API_URL;

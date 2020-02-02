16162f8e9dfa4ad62a82e10e5c0cd20ee3121dc4
Adam <adam.dabrowski@outlook.com>
2020-02-02 11:03:06 +0100

    Render table with data from backend

--- src/components/StockTable/index.tsx
+++ src/components/StockTable/index.tsx
@@ -8,27 +8,53 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+
 interface FetchLoaded<T> {
-  loaded: true,
-  data: T
+  loaded: true;
+  data: T;
 }
 interface FetchWaiting {
-  loaded: false
+  loaded: false;
 }
 type Api<T> = FetchLoaded<T> | FetchWaiting;
 type TickerPrices = Array<TickerPrice>;
 
 function StockTable() {
   const [prices, setData] = useState<Api<TickerPrices>>({
-    loaded: false
+    loaded: false,
   });
   console.log(prices.loaded && prices.data, prices.loaded && prices.data[0]);
   useEffect(() => {
-    getPrices('ENERGA').then(response => setData({loaded: true, data: response}));
+    getPrices('ENERGA').then(response =>
+      setData({loaded: true, data: response}),
+    );
   }, []);
   return (
     <Table>
-      <TableHead>{prices.loaded && prices.data.map((el) => el.name)}Hello</TableHead>
+      <TableHead>
+        <TableRow>
+          <TableCell>Name</TableCell>
+          <TableCell>Date</TableCell>
+          <TableCell>Open</TableCell>
+          <TableCell>Close</TableCell>
+          <TableCell>Min</TableCell>
+          <TableCell>Max</TableCell>
+          <TableCell>Volume</TableCell>
+        </TableRow>
+      </TableHead>
+    <TableBody>
+      {prices.loaded && prices.data.map((ticker, index) => (
+        <TableRow key={index}>
+          <TableCell>{ticker.name}</TableCell>
+          <TableCell>{ticker.date}</TableCell>
+          <TableCell>{ticker.open}</TableCell>
+          <TableCell>{ticker.close}</TableCell>
+          <TableCell>{ticker.min}</TableCell>
+          <TableCell>{ticker.max}</TableCell>
+          <TableCell>{ticker.volume}</TableCell>
+        </TableRow>
+      ))}
+    </TableBody>
     </Table>
   );
 }

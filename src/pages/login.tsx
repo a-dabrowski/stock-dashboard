35630f52fea2d378190526dc149aee655f4f9d31
Adam <adam.dabrowski@outlook.com>
2020-03-15 18:33:59 +0100

    Login page skeleton

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+
 const Login = () => {
   return (
     <div>
       Login Page
+      <form>
+        <TextField
+        id="login"
+        label="Login"
+        variant="outlined"
+        color="secondary"
+      />
+        <TextField
+        id="password"
+        label="Password"
+        variant="outlined"
+        color="secondary"
+      />
+      <Button variant="outlined" color="primary">
+        Login
+    </Button>
+      </form>
     </div>
   );
 }

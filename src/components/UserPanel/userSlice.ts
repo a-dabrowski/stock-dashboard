443f220bd85b4a0e6da959fb7fa2fca73edc65c6
Adam <adam.dabrowski@outlook.com>
2020-03-26 13:06:49 +0100

    Use Redux instead of state for user info management

--- src/components/UserPanel/userSlice.ts
+++ src/components/UserPanel/userSlice.ts
@@ -1,19 +1,49 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { AppThunk } from 'store';
+import { User, getUserData } from "api";
+
+interface iUser {
+  id: number,
+  name: string,
+  subscriptions: string[],
+  displayName: string,
+  isLoggedIn: boolean,
+}
+
+const initialStateUser : iUser = {
+    id: 0,
+  name: 'Skrrt',
+    displayName: 'No custom name',
+    isLoggedIn: true,
+    subscriptions: ['ENERGA', 'PGE'],
+};
 
 export const slice = createSlice({
   name: 'user',
-  initialState: {
-    displayName: 'No custom name',
-    isLoggedIn: true,
-  },
+  initialState: initialStateUser,
   reducers: {
     changeDisplayName: (state, action) => {
       state.displayName = action.payload.newDisplayName
-    }
+    },
+    addFavouriteTicker: (state, action) => {
+      state.subscriptions.push(action.payload);
+    },
+    removeFavouriteTicker: (state, action) => {
+      const target = state.subscriptions.indexOf(action.payload);
+      state.subscriptions = state.subscriptions.splice(target, 1);
+    },
   }
 });
 
 export const selectDisplayName = (state: any) : string => state.user.displayName;
+export const selectUserName = (state: any) : string => state.user.name;
+export const selectFavouriteTickers = (state: any) : string[] => state.user.subscriptions;
 export const selectIsLoggedIn = (state: any) : boolean => state.user.isLoggedIn;
-export const { changeDisplayName } = slice.actions
+export const { changeDisplayName, addFavouriteTicker, removeFavouriteTicker } = slice.actions
 export default slice.reducer;
+
+export const fetchFavouriteTickers = () : AppThunk => async dispatch => {
+  //dispatch loading
+  const fetchedUser = await getUserData(1); //dummy id parameter
+  console.log(fetchedUser);
+};
--- src/pages/user.tsx
+++ src/pages/user.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import User from 'components/UserPanel';
 const UserPanel = () => {
   return (
     <div>
       Change your user identity
+      <User />
     </div>
   );
 }

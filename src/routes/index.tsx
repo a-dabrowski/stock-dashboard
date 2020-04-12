f26b7fdb07cd2a3c0a4d640d0bcf5d87fdf4f7d6
Adam Dąbrowski <adam.dabrowski@publicis.com>
2020-04-12 20:53:24 +0200

    Hide bottom nav for pages not related strictly to app

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import React, {Suspense, lazy} from 'react';
 import {Switch, Route} from 'react-router-dom';
 import StockTable from 'components/StockTable';
 import HeatGraph from 'components/HeatGraph';
+import Navigation from 'components/Navigation';
 
 import Forbidden from 'pages/403';
 import NotFound from 'pages/404';
@@ -13,18 +14,27 @@ const Register = lazy(() => import('pages/register'));
 const SingleStock = lazy(() => import('pages/singleStock'));
 const UserPanel = lazy(() => import('pages/user'));
 
+const NavRoute = ({ component: Component, ...rest }: any) => (
+  <Route {...rest} render={(props: any) => (
+    <div>
+      <Component {...props} />
+      <Navigation />
+    </div>
+  )} />
+)
+
 const routes: React.FC = () => (
   <Suspense fallback={<NotFound />}>
     <Switch>
-      <Route path="/403" component={Forbidden} />
-      <Route path="/analytics" component={Analytics} />
-      <Route path="/heat-graph" component={HeatGraph} />
-      <Route path="/listing" component={Listing} />
-      <Route path="/listing/:id" component={SingleStock} />
-      <Route path="/login" component={Login} />
+      <NavRoute path="/analytics" component={Analytics} />
+      <NavRoute path="/heat-graph" component={HeatGraph} />
+      <NavRoute path="/listing" component={Listing} />
+      <NavRoute path="/listing/:id" component={SingleStock} />
+      <NavRoute path="/stock-table" component={StockTable} />
+      <NavRoute path="/user" component={UserPanel} />
+      <Route exactly path="/login" component={Login} />
       <Route path="/register" component={Register} />
-      <Route path="/stock-table" component={StockTable} />
-      <Route path="/user" component={UserPanel} />
+      <Route path="/403" component={Forbidden} />
       <Route component={NotFound} />
     </Switch>
   </Suspense>

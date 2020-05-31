2a1a0417a2a7099d27f42ecc001f63d9a7983536
Adam <adam.dabrowski@outlook.com>
2020-05-31 13:12:50 +0200

    Smaller margin on top Stock Chart

--- src/components/StockChart/index.tsx
+++ src/components/StockChart/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { ResponsiveLine } from "@nivo/line";
+import Typography from "@material-ui/core/Typography";
 import styled from "styled-components";
 import data from "./data";
 // make sure parent container have a defined height when using
@@ -27,7 +28,7 @@ const Wrapper = styled.div`
 const StockLineChart = (props: any) => (
   <ResponsiveLine
     data={props.data}
-    margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
+    margin={{ top: 10, right: 30, bottom: 50, left: 30 }}
     xScale={{
       type: "time",
       format: "%Y-%m-%d",
@@ -45,6 +46,7 @@ const StockLineChart = (props: any) => (
     axisBottom={{
       format: "%b %d",
       tickValues: "every 1 days",
+      tickRotation: 40,
       legend: "",
       legendOffset: -12
     }}
@@ -81,32 +83,6 @@ const StockLineChart = (props: any) => (
     pointBorderColor={{ from: "serieColor" }}
     pointLabelYOffset={-12}
     useMesh={true}
-    legends={[
-      {
-        anchor: "bottom-right",
-        direction: "column",
-        justify: false,
-        translateX: 100,
-        translateY: 0,
-        itemsSpacing: 0,
-        itemDirection: "left-to-right",
-        itemWidth: 80,
-        itemHeight: 20,
-        itemOpacity: 0.75,
-        symbolSize: 12,
-        symbolShape: "circle",
-        symbolBorderColor: "rgba(0, 0, 0, .5)",
-        effects: [
-          {
-            on: "hover",
-            style: {
-              itemBackground: "rgba(0, 0, 0, .03)",
-              itemOpacity: 1
-            }
-          }
-        ]
-      }
-    ]}
   />
 );
 
